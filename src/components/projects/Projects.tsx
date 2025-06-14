39d1f349bd2a65839e7747a8b0432365823b6965
DESKTOP-S09N3DI\Udhaya <udhaya.shan1@gmail.com>
2025-06-14 15:29:46 +0800

    Add tab switch in projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+import CreateProject from "./CreateProject";
+import ViewProjects from "./ViewProjects";
+import { Tabs } from "antd";
+import { PlusOutlined, FolderOpenOutlined } from "@ant-design/icons";
+
 export default function Projects() {
-  return <h1>Projects</h1>;
+  const tabItems = [
+    {
+      key: "view",
+      label: (
+        <span style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <FolderOpenOutlined />
+          <span>View Projects</span>
+        </span>
+      ),
+      children: <ViewProjects />,
+    },
+    {
+      key: "create",
+      label: (
+        <span style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <PlusOutlined />
+          <span>Create Project</span>
+        </span>
+      ),
+      children: <CreateProject />,
+    },
+  ];
+  return (
+    <>
+      <Tabs items={tabItems}></Tabs>
+    </>
+  );
 }
